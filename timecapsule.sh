a93ef7acf46ae300ff7e864ceb6cbc2a0ebf0162
moeabbass <134096555+moeabbass@users.noreply.github.com>
2023-05-19 14:54:09 -0400

    Add files via upload

---
+++ timecapsule.sh
@@ -0,0 +1,29 @@
+docker run -d --restart=always \
+  --name timemachine \
+  --net=host \
+  -e ADVERTISED_HOSTNAME="" \
+  -e CUSTOM_SMB_CONF="false" \
+  -e CUSTOM_USER="false" \
+  -e DEBUG_LEVEL="1" \
+  -e MIMIC_MODEL="TimeCapsule8,119" \
+  -e EXTERNAL_CONF="" \
+  -e HIDE_SHARES="no" \
+  -e TM_USERNAME="timemachine" \
+  -e TM_GROUPNAME="timemachine" \
+  -e TM_UID="1000" \
+  -e TM_GID="1000" \
+  -e PASSWORD="timemachine" \
+  -e SET_PERMISSIONS="false" \
+  -e SHARE_NAME="TimeMachine" \
+  -e SMB_INHERIT_PERMISSIONS="no" \
+  -e SMB_NFS_ACES="yes" \
+  -e SMB_METADATA="stream" \
+  -e SMB_PORT="445" \
+  -e SMB_VFS_OBJECTS="acl_xattr fruit streams_xattr" \
+  -e VOLUME_SIZE_LIMIT="0" \
+  -e WORKGROUP="WORKGROUP" \
+  -v /path/on/host/to/backup/to/for/timemachine:/opt/timemachine \
+  -v timemachine-var-lib-samba:/var/lib/samba \
+  -v timemachine-var-cache-samba:/var/cache/samba \
+  -v timemachine-run-samba:/run/samba \
+  mbentley/timemachine:smb
